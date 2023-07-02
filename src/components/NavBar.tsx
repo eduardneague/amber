d5039af84bafdb2f8a9e66fe6d27eed3aa4c48be
eduardneague <eduard.neague1337@gmail.com>
2023-07-02 18:58:06 +0300

    margins adjusted

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -29,7 +29,7 @@ const NavBar: React.FC = (): JSX.Element => {
                         initial = {{x: -200, opacity: 0}}
                         animate = {{x: 0, opacity: 1}}
                         transition = {{ease: "easeOut", duration: 1}}
-                        className = "mt-2 nav-logo aspect-square h-24 ml-[2.25rem] cursor-pointer object-contain"
+                        className = "mt-2 nav-logo aspect-square h-24 ml-[1.6rem] cursor-pointer object-contain"
                         src = "amber_logo_full.png" 
                         alt = "amber_logo"
                     />
@@ -37,7 +37,7 @@ const NavBar: React.FC = (): JSX.Element => {
                         initial = {{x: 200, opacity: 0}}
                         animate = {{x: 0, opacity: 1}}
                         transition = {{ease: "easeOut", duration: 1}}
-                        className = "nav-avatar cursor-pointer aspect-square h-10 mr-[2.25rem] object-contain"
+                        className = "nav-avatar cursor-pointer aspect-square h-10 mr-[1.6rem] object-contain"
                         src = "amber_avatar.png" 
                         alt = "avatar_picture"
                     />
