7a7f14c71bf4976b0928f5389d2bda3176cc9408
eduardneague <eduard.neague1337@gmail.com>
2023-07-09 14:31:05 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = (): JSX.Element => {
     useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, (user) => {
         if (user) {
-          // console.log(user)
+          // console.log(user)d
           dispatch(
               login(
                 {
